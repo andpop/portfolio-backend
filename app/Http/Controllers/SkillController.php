0eff1c26db0521a91944c5de6746637b02a37376
Andrey Popov <andpop@mail.ru>
2018-10-14 00:14:20 +0300

    Changed SkillController (added resource methods by artisan).

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -7,12 +7,33 @@ use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $skills = Skill::all();
         return response()->json($skills);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $skill = Skill::add($request->all());
@@ -21,9 +42,48 @@ class SkillController extends Controller
         return response()->json($request->all());
     }
 
-    public function destroy($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Skill $skill)
     {
         Skill::find($id)->delete();
-
     }
 }
